5de9e6a1a8ad058de5f6199a0bf8e97679a89612
Jared Dean <Jared.Dean@sas.com>
2015-07-13 08:03:10 -0400

    Refactor kernel.py to use metakernel
    Metakernel gives many advantages including magics
    simplier code completion and so on.

--- sas_kernel/kernel.py
+++ sas_kernel/kernel.py
@@ -13,7 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-from IPython.kernel.zmq.kernelbase import Kernel
+#from IPython.kernel.zmq.kernelbase import Kernel
 from IPython.display import HTML, display
 from IPython.display import Image
 from pexpect import replwrap, EOF
@@ -34,43 +34,30 @@ __version__ = '0.1'
 
 version_pat = re.compile(r'version (\d+(\.\d+)+)')
 
+from metakernel import MetaKernel
 
-#from .images import (
-#    extract_image_filenames, display_data_for_image, image_setup_cmd
-#)
-
-
-class SASKernel(Kernel):
+class SASKernel(MetaKernel):
     implementation = 'sas_kernel'
-    implementation_version = __version__
-
-    @property
-    def language_version(self):
-        m = version_pat.search(self.banner)
-        return m.group(1)
-
-    _banner = None
-
-    @property
-    def banner(self):
-        if self._banner is None:
-            self._banner = check_output(['/root/anaconda3/bin/python3.4', '--version']).decode('utf-8')
-        return self._banner
-
+    implementation_version = '1.0'
+    language = 'text'
+    language_version = '0.1'
+    banner = "SAS Kernel"
     language_info = {'name': 'sas',
-                     #'codemirror_mode': 'shell',
-                     'mimetype': 'text/plain',
                      'file_extension': '.sas'}
 
-    def __init__(self, **kwargs):
-        Kernel.__init__(self, **kwargs)
+    def __init__(self,**kwargs):
+
+        MetaKernel.__init__(self, **kwargs)
         self._start_sas()
+
+    def get_usage(self):
+        return "This is the SAS kernel."
 
     def _start_sas(self):
         # Signal handlers are inherited by forked processes, and we can't easily
         # reset it from the subprocess. Since kernelapp ignores SIGINT except in
         # message handlers, we need to temporarily reset the SIGINT handler here
-        # so that sas and its children are interruptible.
+        # so that SAS and its children are interruptible.
         sig = signal.signal(signal.SIGINT, signal.SIG_DFL)
         try:
             #create a shell session
@@ -87,18 +74,16 @@ class SASKernel(Kernel):
         # Register sas function to write image data to temporary file
         #self.saswrapper.run_command(image_setup_cmd)
 
-    def do_execute(self, code, silent, store_history=True,
-                   user_expressions=None, allow_stdin=False):
-        
+    def do_execute_direct(self, code):
+
         if not code.strip():
             return {'status': 'ok', 'execution_count': self.execution_count,
                     'payload': [], 'user_expressions': {}}
- 
         interrupted = False
         submit_pre=str('mva.submit("')
         submit_post=str('","html")')
-        sas_log=str('mva.getlog()')
-        sas_lst=str('mva.getlst()')
+        sas_log='mva.getlog()'
+        sas_lst='mva.getlst()'
 
         #remove whitespace characters
         remap = {
@@ -107,9 +92,8 @@ class SASKernel(Kernel):
             ord('\r') : None,
             ord('\n') : None
         }
-        #code='proc print data=sashelp.class; run;'
         try:
-            rc = self.saswrapper.run_command(submit_pre + code.translate(remap) + submit_post, timeout=None)
+            rc = self.saswrapper.run_command(submit_pre + code.translate(remap) + submit_post, timeo
             time.sleep(.5)
             # block until log send EOF
             # blocking is done automatically for HTML output b/c it can look for closing html tag
@@ -119,98 +103,66 @@ class SASKernel(Kernel):
             output=self.saswrapper.run_command(sas_lst,timeout=None)
             #if output.startswith('\''):
             #   re.sub(r'^\'',r'^', output)
-            #   print ("startswith")
-            if output.endswith('\''):
-               re.sub(r'\'$',r'$', output)
-               print("endswith")
-            self.log.debug('execute: %s' % code)
-
-
-            print ('code: ' + submit_pre + code.translate(remap) + submit_post)
-            print ('rc: ' + rc)
+            #if output.endswith('\''):
+            #   re.sub(r'\'$',r'$', output)
+            #self.log.debug('execute: %s' % code)
             type(log)
-            print ("Output type: " + str(type(output)))
-            print('Silent: ' + str(silent))
-        except KeyboardInterrupt:
-            self.saswrapper.child.sendintr()
-            interrupted = True
-            self.saswrapper._expect_prompt()
-            output = self.saswrapper.child.before
+
         except EOF:
             output = self.saswrapper.child.before + 'Restarting SAS'
             self._start_sas()
+
+        output = output.replace('\\n', chr(10)).replace('\\r',chr(ord('\r'))).replace('\\t',chr(ord('\t'))).replace('\\f',chr(ord('\f')))
+        newcontent={'source':"Kernel",'data':{'text/plain':'<IPython.core.display.HTML object>','text/html': output},'metadata':{}}
         
-        if len(output)>0: #not silent:
-            output = output.replace('\\n', chr(ord('\n'))).replace('\\r',chr(ord('\r'))).replace('\\t',chr(ord('\t'))).replace('\\f',chr(ord('\f')))
-            #print(output)
-            newcontent={'source':'Kernel','data':{'text/plain':'<IPython.core.display.HTML object>','text/html': output},'metadata':{}}
-            #newcontent={'source':"Kernel",'data':{'text/html': 'output'},'metadata':{}}
-            self.send_response(self.iopub_socket, 'display_data', newcontent)
-            print ("Output type (not silent): " + str(type(output)))
+        return HTML(output)
 
-        else:
-            log = log.replace('\\n', chr(ord('\n'))).replace('\\r',chr(ord('\r'))).replace('\\t',chr(ord('\t'))).replace('\\f',chr(ord('\f')))
-            #print(output)
-            newcontent={'source':'Kernel','data':{'text/plain':'<IPython.core.display.HTML object>','text/html': log},'metadata':{}}
-            #newcontent={'source':"Kernel",'data':{'text/html': 'output'},'metadata':{}}
-            self.send_response(self.iopub_socket, 'display_data', newcontent)
-
-            
-
-        if interrupted:
-            return {'status': 'abort', 'execution_count': self.execution_count}
-
-        try:
-            exitcode = int(self.saswrapper.run_command('&syserr;').rstrip())
-        except Exception:
-            exitcode = 1
-
-        if exitcode:
-            return {'status': 'error', 'execution_count': self.execution_count,
-                    'ename': '', 'evalue': str(exitcode), 'traceback': []}
-        else:
-            return {'status': 'ok', 'execution_count': self.execution_count,
-                    'payload': [], 'user_expressions': {}}
     #Get code complete file from EG for this
-    def do_complete(self, code, cursor_pos):
-        code = code[:cursor_pos]
-        default = {'matches': [], 'cursor_start': 0,
-                   'cursor_end': cursor_pos, 'metadata': dict(),
-                   'status': 'ok'}
+    def get_completions(self,info):
+        list=['proc','print','optmodel','data']
+        potentials=get_close_matches(info['obj'],list)
+        return potentials
 
-        if not code or code[-1] == ' ':
-            return default
+    
+    # def do_complete(self, code, cursor_pos):
+    #     code = code[:cursor_pos]
+    #     default = {'matches': [], 'cursor_start': 0,
+    #                'cursor_end': cursor_pos, 'metadata': dict(),
+    #                'status': 'ok'}
 
-        tokens = code.replace(';', ' ').split()
-        if not tokens:
-            return default
+    #     if not code or code[-1] == ' ':
+    #         return default
 
-        matches = []
-        token = tokens[-1]
-        start = cursor_pos - len(token)
+    #     tokens = code.replace(';', ' ').split()
+    #     if not tokens:
+    #         return default
 
-        if token[0] == '$':
-            # complete variables
-            cmd = 'compgen -A arrayvar -A export -A variable %s' % token[1:] # strip leading $
-            output = self.saswrapper.run_command(cmd).rstrip()
-            completions = set(output.split())
-            # append matches including leading $
-            matches.extend(['$'+c for c in completions])
-        else:
-            # complete functions and builtins
-            cmd = 'compgen -cdfa %s' % token
-            output = self.saswrapper.run_command(cmd).rstrip()
-            matches.extend(output.split())
+    #     matches = []
+    #     token = tokens[-1]
+    #     start = cursor_pos - len(token)
+
+    #     if token[0] == '$':
+    #         # complete variables
+    #         cmd = 'compgen -A arrayvar -A export -A variable %s' % token[1:] # strip leading $
+    #         output = self.saswrapper.run_command(cmd).rstrip()
+    #         completions = set(output.split())
+    #         # append matches including leading $
+    #         matches.extend(['$'+c for c in completions])
+    #     else:
+    #         # complete functions and builtins
+    #         cmd = 'compgen -cdfa %s' % token
+    #         output = self.saswrapper.run_command(cmd).rstrip()
+    #         matches.extend(output.split())
             
-        if not matches:
-            return default
-        matches = [m for m in matches if m.startswith(token)]
+    #     if not matches:
+    #         return default
+    #     matches = [m for m in matches if m.startswith(token)]
 
-        return {'matches': sorted(matches), 'cursor_start': start,
-                'cursor_end': cursor_pos, 'metadata': dict(),
-                'status': 'ok'}
-
-    def do_shutdown(restart):
+    #     return {'matches': sorted(matches), 'cursor_start': start,
+    #             'cursor_end': cursor_pos, 'metadata': dict(),
+    #             'status': 'ok'}
+    
+    def do_shutdown(self,restart):
         if restart:
             self.saswrapper.run_command('mva.submit(";endsas;")')
 
