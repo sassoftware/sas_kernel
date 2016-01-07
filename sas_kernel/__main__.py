1cc510efd50e448a5190176b4ff2846db3c4b330
Jared Dean <Jared.Dean@sas.com>
2016-01-07 15:32:56 -0500

    remove shim warnings from deprecated packages
    Former-commit-id: 56929e66f5e9cadf0b77b844aa1d4a43061d875d

--- sas_kernel/__main__.py
+++ sas_kernel/__main__.py
@@ -1,3 +1,8 @@
-from IPython.kernel.zmq.kernelapp import IPKernelApp
+#import warnings
+#from  IPython.utils.shimmodule import ShimWarning
+#warnings.filterwarnings('error','', ShimWarning)
+
+
+from ipykernel.kernelapp import IPKernelApp
 from .kernel import SASKernel
 IPKernelApp.launch_instance(kernel_class=SASKernel)
