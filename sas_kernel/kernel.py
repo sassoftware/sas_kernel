011779614a25ea643d02b56b487aad236a8f462f
Jared Dean <Jared.Dean@sas.com>
2016-01-08 16:25:11 +0000

    env updates
    Former-commit-id: 1d4a6e2ca726c16abf8179939fbbc68b2417cecf [formerly 01ab0efd0fd7f42398f3bb310ba05fa0edb1d31c]
    Former-commit-id: de8d781e5fa5ccd1ec2883d9a20f3153d6475afb

--- sas_kernel/kernel.py
+++ sas_kernel/kernel.py
@@ -16,7 +16,7 @@ from saspy.SASLogLexer import *
 #Create Logger
 import logging
 logger= logging.getLogger('')
-logger.setLevel(logging.WARN)
+logger.setLevel(logging.DEBUG)
 
 
 __version__ = '0.1'
@@ -47,6 +47,12 @@ class SASKernel(MetaKernel):
         self.strproclist='\n'.join(str(x) for x in self.proclist)
         MetaKernel.__init__(self, **kwargs)
         self.mva = None
+        executable = os.environ.get('SAS_EXECUTABLE', 'sas')
+        if executable=='sas':
+            executable='/opt/sasinside/SASHome/SASFoundation/9.4/sas'
+        e2=executable.split('/')
+        self._path='/'.join(e2[0:e2.index('SASHome')+1])
+        self._version=e2[e2.index('SASFoundation')+1] 
         self._start_sas()
 
     def get_usage(self):
@@ -62,8 +68,9 @@ class SASKernel(MetaKernel):
         try:
             # start a SAS session within python bound to the shell session
             import saspy as saspy
+            print("In _start_sas" + self._path + self._version)
             self.mva=saspy.SAS_session()
-            self.mva._startsas()
+            self.mva._startsas(path=self._path, version=self._version)
         finally:
             signal.signal(signal.SIGINT, sig)
 
