b4fd2c6b5d7976a8fb0e91b0a5f52acc5520e716
Jared Dean <Jared.Dean@sas.com>
2016-01-08 16:25:11 +0000

    env updates
    Former-commit-id: bce450938a1a6d3b3568169b8ee3378c9f7e8a00 [formerly 87c13a826832528512b2d49dfe696191c3446871]
    Former-commit-id: f526a72a661979bc755cc978e755d558994d2dd4

--- sas_kernel/kernel.py
+++ sas_kernel/kernel.py
@@ -16,7 +16,7 @@ from saspy.SASLogLexer import *
 #Create Logger
 import logging
 logger= logging.getLogger('')
-logger.setLevel(logging.WARN)
+logger.setLevel(logging.DEBUG)
 
 
 __version__ = '0.1'
@@ -47,6 +47,12 @@ class SASKernel(MetaKernel):
         self.strproclist='\n'.join(str(x) for x in self.proclist)
         MetaKernel.__init__(self, **kwargs)
         self.mva = None
+        executable = os.environ.get('SAS_EXECUTABLE', 'sas')
+        if executable=='sas':
+            executable='/opt/sasinside/SASHome/SASFoundation/9.4/sas'
+        e2=executable.split('/')
+        self._path='/'.join(e2[0:e2.index('SASHome')+1])
+        self._version=e2[e2.index('SASFoundation')+1] 
         self._start_sas()
 
     def get_usage(self):
