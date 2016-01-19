c8999c0669d56ac4424f2f5b76bcb1ab6c096e95
Jared Dean <Jared.Dean@sas.com>
2016-01-19 13:23:50 -0500

    fix saspy invocation

--- sas_kernel/magics/sas_magic.py
+++ sas_kernel/magics/sas_magic.py
@@ -50,6 +50,9 @@ class SASMagic(ipym.Magics):
            e=diag({1 2, 3 4});
 
         '''
+        import saspy as saspy
+        self.mva=saspy.SAS_session()
+        self.mva._startsas()#path=self._path, version=self._version)
         res=sas.submit("proc iml; "+ self.code + " quit;")
         output=_clean_output(res['LST'])
         log=_clean_log(res['LOG'])
@@ -86,6 +89,9 @@ class SASMagic(ipym.Magics):
         quit;
 
         '''
+        import saspy as saspy
+        self.mva=saspy.SAS_session()
+        self.mva._startsas()#path=self._path, version=self._version)
         res=sas.submit("proc optmodel; "+ self.code + " quit;")
         output=_clean_output(res['LST'])
         log=_clean_log(res['LOG'])
