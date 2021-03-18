9ef8995a5b189429733cdcbe72365fb8af1e0b57
Jared Dean <jared.dean@sas.com>
2021-03-18 15:56:11 -0400

    set sascfg http time out to 30 seconds.

--- binder/sascfg_personal.py
+++ binder/sascfg_personal.py
@@ -3,5 +3,6 @@ SAS_config_names = ['viya4']
 viya4 = {
     'url':'https://d30818.ingress-nginx.jadean1.perf.sas.com:443',
     'verify': False,
+    'timeout': 30,
     'context':'SAS Job Execution compute context'
     }
