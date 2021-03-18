a17ec4b3d6ffbdabefcc6905b152bc9313d74381
Jared Dean <jared.dean@sas.com>
2021-03-18 11:52:10 -0400

    http sascfg test

---
+++ binder/sascfg_personal.py
@@ -0,0 +1,7 @@
+SAS_config_names = ['viya4']
+
+viya4 = {
+    'url':'https://d30818.ingress-nginx.jadean1.perf.sas.com:443',
+    'verify': False,
+    'context':'SAS Job Execution compute context'
+    }
