5c22bec46f77232e9985bc355acbe1d41200bdf4
Jared Dean <jared.dean@sas.com>
2021-03-19 10:09:05 -0400

    temp url while waiting for final location

--- binder/sascfg_personal.py
+++ binder/sascfg_personal.py
@@ -1,7 +1,7 @@
 SAS_config_names = ['viya4']
 
 viya4 = {
-    'url':'https://d30818.ingress-nginx.jadean1.perf.sas.com:443',
+    'url':'https://temp.url.com:443',
     'verify': False,
     'timeout': 30,
     'context':'SAS Job Execution compute context'
