2f5f7f12bc67691f0e8d87ba026b46244ef66bd4
Jared Dean <jared.dean@sas.com>
2021-06-11 12:18:41 -0400

    Update binder details for connection.

--- binder/sascfg_personal.py
+++ binder/sascfg_personal.py
@@ -1,8 +1,10 @@
 SAS_config_names = ['viya4']
 
 viya4 = {
-    'url':'https://temp.url.com:443',
+    'url':'https://binder.demo.sas.com',
     'verify': False,
-    'timeout': 30,
-    'context':'SAS Job Execution compute context'
+    'context':'SAS Job Execution compute context',
+    'client_id'     : "SASPy",
+    'client_secret' : "7c301751-3114-4554-ad82-e20e3466bb17",
+
     }
